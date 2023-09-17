54875ea785dab6368eadd56f06c9bc07dae49cb8
GammaGammaRay <benjamin.peret@gmail.com>
2023-09-16 22:53:36 -0300

    fix: page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
 import React from "react";
 import Nav from "./components/Nav.component.jsx";
 import Lenis from "@studio-freight/lenis";
-// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
-import Logo from "./pages/0Logo.page.jsx";
-import QuemSomos from "./pages/1QuemSomos.page.jsx";
-import { BodyContainer } from "./style/PageContainers";
-import ComoFunciona from "./pages/2ComoFunciona.page.jsx";
-import Contribuicao from "./pages/3ContribuicaoInfo.page.jsx";
-import Buscamos from "./pages/4Buscamos.page.jsx";
-import Historico from "./pages/5Historico.jsx.jsx";
-import Balanco from "./pages/6Balanco.page.jsx";
-import Contato from "./pages/7Contato.page.jsx";
+import { BodyContainer, Section } from "./style/PageContainers";
+
+import styled from "styled-components";
 
 function App() {
   return (
     <React.Fragment>
       <Nav />
       <BodyContainer>
-        <Logo />
-        <QuemSomos />
-        <ComoFunciona />
-        <Contribuicao />
-        <Buscamos />
-        <Historico />
-        <Balanco />
-        <Contato />
+        <LogoSection>
+          <img
+            className="logo"
+            type="image/svg+xml"
+            src="chao_logo_preto-no-branco.svg"
+            alt="Logo Instituto Chão"
+          />
+        </LogoSection>
       </BodyContainer>
     </React.Fragment>
   );
 }
+
+const LogoSection = styled(Section)`
+  background-color: black;
+  width: 100%;
+  img {
+    height: 800px;
+  }
+`;
 
 export default App;
