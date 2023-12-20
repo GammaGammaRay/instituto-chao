a5ba043bf49b08adbf6784259e7f84982e3f377a
GammaGammaRay <benjamin.peret@gmail.com>
2023-12-20 02:01:39 -0300

    fix: bg

--- src/pages/1QuemSomos.page.jsx
+++ src/pages/1QuemSomos.page.jsx
@@ -161,58 +161,60 @@ const QuemSomosContainer = styled(ContentContainer)`
     justify-content: start;
     align-items: start;
     width: 60%;
-    font-family: var(--title-font);
-    font-weight: var(--title-font-weight);
-    font-size: var(--title-font-size-horz);
-    height: fit-content;
-    line-height: 0.8em;
-    word-wrap: break-word;
 
-    display: flex;
-    flex-direction: row;
-    align-items: start;
-    flex-basis: 0;
-    writing-mode: vertical-lr;
-
-    text-align: end;
-    background-color: lightcyan;
-    will-change: transform;
-
-    @media (min-width: 768px) {
-      width: 100%;
-      height: fit-content;
-      writing-mode: vertical-lr;
-      text-orientation: sideways;
-      flex-direction: column;
-      align-items: start;
-      justify-content: start;
-      flex-basis: 0;
-      font-size: var(--title-font-size-vert);
-      will-change: transform;
-    }
-
-    span {
-      overflow: visible;
-
-      @media (min-width: 768px) {
-        transform: rotate(180deg);
-      }
-    }
-
-    &.sticky {
-      /* position: absolute; */
-      /* z-index: -1; */
-      top: 0;
-      background-color: white;
-    }
-
-    /* position: sticky; */
     display: flex;
     flex-direction: row;
     justify-content: center;
   }
 `;
 
-const QuemSomosTitle = styled(VerticalTitle)``;
+const QuemSomosTitle = styled(VerticalTitle)`
+  font-family: var(--title-font);
+  font-weight: var(--title-font-weight);
+  font-size: var(--title-font-size-horz);
+  height: fit-content;
+  line-height: 0.8em;
+  word-wrap: break-word;
+
+  display: flex;
+  flex-direction: row;
+  align-items: start;
+  flex-basis: 0;
+  writing-mode: vertical-lr;
+
+  text-align: end;
+  background-color: lightcyan;
+  will-change: transform;
+
+  @media (min-width: 768px) {
+    width: 100%;
+    height: fit-content;
+    writing-mode: vertical-lr;
+    text-orientation: sideways;
+    flex-direction: column;
+    align-items: start;
+    justify-content: start;
+    flex-basis: 0;
+    font-size: var(--title-font-size-vert);
+    will-change: transform;
+  }
+
+  span {
+    overflow: visible;
+
+    @media (min-width: 768px) {
+      transform: rotate(180deg);
+    }
+  }
+
+  &.sticky {
+    /* position: absolute; */
+    /* z-index: -1; */
+    top: 0;
+    background-color: white;
+  }
+
+  /* position: sticky; */
+`;
 
 export default QuemSomos;
